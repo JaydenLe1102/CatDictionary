70e7da5b9d7409d05b266f15aa9bec31d2ca105d
Jayden <nhukhang1102@gmail.com>
2022-02-18 15:15:10 -0800

    changing mainactivity to feed activity

--- app/src/main/java/com/example/cat_dictionary_app/FeedActivity.kt
+++ app/src/main/java/com/example/cat_dictionary_app/FeedActivity.kt
@@ -3,7 +3,7 @@ package com.example.cat_dictionary_app
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class FeedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
