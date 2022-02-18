436ee03ba77f68a272715ee76dc08c8cc2328eef
nguyencongphap <nguyencongphap@yahoo.com>
2022-02-17 19:11:54 -0800

    Added bottom navigation bar, empty fragments

---
+++ app/src/main/java/com/example/cat_dictionary_app/fragments/HomeFragment.kt
@@ -0,0 +1,25 @@
+package com.example.cat_dictionary_app.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.cat_dictionary_app.R
+
+
+class HomeFragment : Fragment() {
+
+    // onCreateView of Fragment is similar to onCreate of Activity
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    companion object {
+
+    }
+}
